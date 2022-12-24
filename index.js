baf15d93b739460de76449bda33190b50d9176d5
Ha Young Roh <hyroh795@gmail.com>
2022-12-24 20:03:16 +0900

    commit tree 30bf8aa054e3c50c02310059adcca98f4a2ef38e

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let endIndex = word.length-1
+  for(let i=0; i<word.length; i++){
+    //console.log('inside for loop')
+    //console.log(`i value: ${i}`)
+   // console.log(`endIndex value: ${endIndex}`)
+    //console.log(word[i])
+    //console.log(word[endIndex])
+  if (word[i]===word[endIndex]){
+    endIndex --;
+  }else{
+    return false;
+  }
+    }
+ return true;
 }
 
 /* 
   Add your pseudocode here
+
+  check the first element and 
+  the last element char has the same char
+  
+  mom
+  first element: 0
+  last elementt: mom.length()=3-1=2
+
+  next element: 1
+  last element: 1 
+
 */
 
 /*
